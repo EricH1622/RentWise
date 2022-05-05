b10797c90dcba4d8d66ba1bd3d3c18958ad0a846
james01123 <erichemming11@gmail.com>
2022-05-05 13:29:09 -0700

    Updated nameing of database tables and added user_role column

--- database.sql
+++ database.sql
@@ -8,15 +8,15 @@ CREATE TABLE IF NOT EXISTS `BBY_37_user` (
   `last_name` varchar(100) NOT NULL,
   `email_address` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `role` varchar(50) NOT NULL,
+  `role_id` int(2) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role`) VALUES (1, 'sam_a', 'Sam', 'A', 'sam@email.com', 'password1', 'user');
-INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role`) VALUES (2, 'eric_d', 'Eric', 'D', 'eric_d@email.com', 'password2', 'admin');
-INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role`) VALUES (3, 'eric_h', 'Eric', 'H', 'eric@email.com', 'password3', 'user');
-INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role`) VALUES (4, 'kiefer_t', 'Kiefer', 'T', 'keifer@email.com', 'password4', 'admin');
-INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role`) VALUES (5, 'will_w', 'Will', 'W', 'will@email.com', 'password5', 'user');
+INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (1, 'sam_a', 'Sam', 'A', 'sam@email.com', 'password1', 0);
+INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (2, 'eric_d', 'Eric', 'D', 'eric_d@email.com', 'password2', 1);
+INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (3, 'eric_h', 'Eric', 'H', 'eric@email.com', 'password3', 0);
+INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (4, 'kiefer_t', 'Kiefer', 'T', 'keifer@email.com', 'password4', 1);
+INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (5, 'will_w', 'Will', 'W', 'will@email.com', 'password5', 0);
 
 
 -- CREATE TABLE IF NOT EXISTS `BBY_37_roles` (
