045442aba4ed6bd4b7b43aff5d2261809cb664dd
Sam A <maghaebrahimi@my.bcit.ca>
2022-05-06 13:32:21 -0700

    server now adds table and personal info into page

--- Server.js
+++ Server.js
@@ -6,6 +6,7 @@ const session = require("express-session");
 const app = express();
 const fs = require("fs");
 const mysql = require("mysql2/promise");
+const { JSDOM } = require('jsdom');
 
 
 app.use("/js", express.static("./js"));
@@ -70,27 +71,95 @@ app.get("/logout", function (req, res) {
 
 
 app.get("/profile", function (req, res) {
+  sendProfilePage(req, res);
+});
+
+async function sendProfilePage(req, res) {
   let doc = 0;
+  let docDOM = 0;
   if (req.session.loggedIn) {
+
+    // regular user, do:
     if (req.session.userlevel == 0) {
       doc = fs.readFileSync("./html/profile.html", "utf8");
-      res.send(doc);  
+      docDOM = new JSDOM(doc);
+
+      const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "COMP2800",
+        multipleStatements: true
+      });
+      connection.connect();
+      const [rows, fields] = await connection.execute(
+        "SELECT username, email_address, password FROM BBY_37_user " +
+        "WHERE BBY_37_user.user_id = " + req.session.userid);
+      await connection.end();
+
+      docDOM.window.document.getElementById("username").innerHTML = "Username: " + rows[0].username;
+      docDOM.window.document.getElementById("email").innerHTML = "Email: " + rows[0].email_address;
+      docDOM.window.document.getElementById("password").innerHTML = "Password: " + rows[0].password;
+      res.send(docDOM.serialize());
+
+
+      // admin user, do:
     } else if (req.session.userlevel == 1) {
       doc = fs.readFileSync("./html/admin.html", "utf8");
-      res.send(doc);  
+      docDOM = new JSDOM(doc);
+
+      const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "COMP2800",
+        multipleStatements: true
+      });
+      connection.connect();
+      let [rows, fields] = await connection.execute(
+        "SELECT username, email_address, password FROM BBY_37_user " +
+        "WHERE BBY_37_user.user_id = " + req.session.userid);
+
+      docDOM.window.document.getElementById("username").innerHTML = "Username: " + rows[0].username;
+      docDOM.window.document.getElementById("email").innerHTML = "Email: " + rows[0].email_address;
+      docDOM.window.document.getElementById("password").innerHTML = "Password: " + rows[0].password;
+
+      [rows, fields] = await connection.execute("SELECT * FROM BBY_37_user ");
+      await connection.end();
+      // `user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`
+
+      let table = "<table><tr>" +
+                  "<th>ID</th>" +
+                  "<th>Username</th>" +
+                  "<th>First Name</th>" +
+                  "<th>Last Name</th>" +
+                  "<th>Email</th>" +
+                  "<th>Password</th>" +
+                  "<th>User Type</th></tr>";
+
+      for (let i = 0; i < rows.length; i++) {
+        table += "<tr><td>" +
+                  rows[i].user_id + "</td><td>" +
+                  rows[i].username + "</td><td>" +
+                  rows[i].first_name + "</td><td>" +
+                  rows[i].last_name + "</td><td>" +
+                  rows[i].email_address + "</td><td>" +
+                  rows[i].password + "</td><td>" +
+                  rows[i].role_id + "</td></tr>";
+      }
+      table += "</table>";
+
+      docDOM.window.document.getElementById("tableContainer").innerHTML = table;
+      res.send(docDOM.serialize());
     }
   } else {
     // not logged in - no session and no access, redirect to root.
     res.redirect("/");
   }
-});
+}
 
 
 app.post("/login", function (req, res) {
-  res.setHeader("Content-Type", "application/json");
-  // console.log("What was sent", req.body.email, req.body.password);
-
-  //check with server
   authenticateUser(req, res);
 });
 
@@ -109,7 +178,8 @@ async function authenticateUser(req, res) {
       [req.body.username, req.body.password]);
 
   await connection.end();
-      
+
+  res.setHeader("Content-Type", "application/json");
   if (rows.length == 1 ) {
       // user authenticated, create a session
       req.session.loggedIn = true;
