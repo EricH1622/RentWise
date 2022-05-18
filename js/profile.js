f176d0b62ce6f13123930c629b0e7ce599b2f841
Will Whitfield <willowhitfield@outlook.com>
2022-05-18 13:25:10 -0700

    Fully implemented easter egg, now triggers off of 7 presses of the "copyright" section in a short period of time, and pulls up a panel with a blooper real from our pitch video.

--- js/profile.js
+++ js/profile.js
@@ -31,28 +31,49 @@ ready(document.getElementById("editButton").addEventListener("click", function(e
   }
 }))
 
-//Easter egg :)
+//Easter egg code :)
 let eggActive = false;
 let timesClicked = 0;
-ready(document.getElementById("logo1").addEventListener("click", function(e) {
+ready(document.getElementById("copyright").addEventListener("click", function(e) {
   let timer;
   if(eggActive == false){
     eggActive = true;
-    console.log("easter egg trigger started!");
     timer = setTimeout(function(){
       eggActive = false;
+      if (timesClicked < 7) {
+        for (let i = 1; i <= 7; i++) {
+          document.getElementById("c" + i).style.color = "white";
+        }
+      }
       timesClicked = 0;
-      console.log("time's up!");
     }, 1500)
   }
   timesClicked++;
-  console.log(timesClicked);
   if(timesClicked >= 7){
-    clearTimeout(timer);
-    console.log("easter egg is activated!");
-    timesClicked = 0;
+    document.getElementById("c7").style.color = "rgb(255, 157, 0)";
+    document.getElementById("easterEgg").style.display = "grid";
+    //Don't want to display the easter egg stuff in the div before the user triggers it, but if a savvy user sees the div with nothing in it they might be incentivized to try to search for the trigger
+    document.getElementById("easterEgg").innerHTML = `<img src="/assets/images/closeButton.png" id="closeEgg"/>
+    <iframe id="easterEggVideo" src="https://www.youtube-nocookie.com/embed/9ti1k2tnDTM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h1>...It looks like you've found something!</h1>`;
+
+    document.getElementById("closeEgg").addEventListener("click", function(e) {
+      document.getElementById("easterEgg").style.display = "none";
+      document.getElementById("easterEgg").innerHTML = "";
+      eggActive = false;
+      for (let i = 1; i <= 7; i++) {
+        document.getElementById("c" + i).style.color = "white";
+      }
+    });
+
+  } else{
+    document.getElementById("c" + timesClicked).style.color = "rgb(255, 157, 0)";
   }
 }))
+
+//Easter egg code is over now
+
+
 
 function ready(callback) {
   if (document.readyState != "loading") {
