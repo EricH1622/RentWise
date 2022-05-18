5c7e0c6fbf0417644d3ed9719b823777a7e5b555
Eric H <erichemming11@gmail.com>
2022-05-18 13:51:45 -0700

    Added last_edited_date column to BBY_27_post table.

--- database.sql
+++ database.sql
@@ -42,7 +42,8 @@ INSERT INTO `BBY_37_location` (`location_id`, `street_number`, `prefix`, `street
 CREATE TABLE IF NOT EXISTS `BBY_37_post` (
   `post_id` int(16) NOT NULL AUTO_INCREMENT,
   `user_id` int(16) NOT NULL,
-  `timestamp` DATETIME NOT NULL,
+  `date_created` TIMESTAMP NOT NULL,
+  `last_edited_date` TIMESTAMP,
   `content` BLOB,
   `location_id` int(16) NOT NULL,
   `photo1` varchar(1024),
@@ -53,8 +54,8 @@ CREATE TABLE IF NOT EXISTS `BBY_37_post` (
   FOREIGN KEY(location_id) REFERENCES BBY_37_location(location_id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `timestamp`, `content`, `location_id`) VALUES (1, 1, '2022-05-10 13:23:44', 'Great property the landlord was always nice and very responsive.', 1);
-INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `timestamp`, `content`, `location_id`) VALUES (2, 1, '2022-05-10 14:25:51', 'I lived there for 2 years, the neighbors were terrible and always loud. Other than that the landlord was great and everything worked.', 2);
-INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `timestamp`, `content`, `location_id`) VALUES (3, 2, '2022-05-10 11:21:03', 'The location is great, a little close the the Skytrain so there is some background noise from the trains when they pass by.', 3);
-INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `timestamp`, `content`, `location_id`) VALUES (4, 3, '2022-05-10 16:53:55', 'Awesome place, very peaceful and quiet.', 4);
-INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `timestamp`, `content`, `location_id`) VALUES (5, 1, '2022-05-10 17:16:23', 'I enjoyed the house, but the landlord was unresponsive and never fixed anything, then they tried to increase myrent at the end of the year. Had to move out because I could not deal with them', 5);
+INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`) VALUES (1, 1, '2022-05-10 13:23:44', 'Great property the landlord was always nice and very responsive.', 1);
+INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`) VALUES (2, 1, '2022-05-10 14:25:51', 'I lived there for 2 years, the neighbors were terrible and always loud. Other than that the landlord was great and everything worked.', 2);
+INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`) VALUES (3, 2, '2022-05-10 11:21:03', 'The location is great, a little close the the Skytrain so there is some background noise from the trains when they pass by.', 3);
+INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`) VALUES (4, 3, '2022-05-10 16:53:55', 'Awesome place, very peaceful and quiet.', 4);
+INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`) VALUES (5, 1, '2022-05-10 17:16:23', 'I enjoyed the house, but the landlord was unresponsive and never fixed anything, then they tried to increase myrent at the end of the year. Had to move out because I could not deal with them', 5);
