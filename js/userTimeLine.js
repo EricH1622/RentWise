df125ab3bedd27aa7e88fb188d437e2786bad2c4
vagabondtypewriter <kieferthominquiries@gmail.com>
2022-05-18 21:30:32 -0700

    added code logic as TODO code

--- js/userTimeLine.js
+++ js/userTimeLine.js
@@ -1,5 +1,11 @@
-// load data from the database onto the page 
-// add a div of a class for each item 
+// load data from the database onto the page
+// - query database for all posts that the user has written
+// - set a counter variable to that amount
+// - for each (in counter) create a div and add further queried information
+
+// SELECT * FROM bby_37_post WHERE user_id(post) = user_id(user)
+
+// add a div of a class for each item
 // add data to div
 // add div to html under history
 
