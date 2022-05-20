fd0ea176b295e66994e5ef95ef063cdb93c83585
james01123 <erichemming11@gmail.com>
2022-05-20 13:13:13 -0700

    Added default placeholder photo path for posts in photo1column of BBY_37_post table

--- database.sql
+++ database.sql
@@ -13,11 +13,11 @@ CREATE TABLE IF NOT EXISTS `BBY_37_user` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (1, 'sam_a', 'Sam', 'A', 'sam@email.com', 'password1', 0);
--- INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (2, 'eric_d', 'Eric', 'D', 'eric_d@email.com', 'password2', 1);
--- INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (3, 'eric_h', 'Eric', 'H', 'eric@email.com', 'password3', 0);
--- INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (4, 'kiefer_t', 'Kiefer', 'T', 'keifer@email.com', 'password4', 1);
--- INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (5, 'will_w', 'Will', 'W', 'will@email.com', 'password5', 0);
+INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (1, 'sam_a', 'Sam', 'A', 'sam@email.com', 'password1', 0);
+INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (2, 'eric_d', 'Eric', 'D', 'eric_d@email.com', 'password2', 1);
+INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (3, 'eric_h', 'Eric', 'H', 'eric@email.com', 'password3', 0);
+INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (4, 'kiefer_t', 'Kiefer', 'T', 'keifer@email.com', 'password4', 1);
+INSERT INTO `BBY_37_user` (`user_id`, `username`, `first_name`, `last_name`, `email_address`, `password`, `role_id`) VALUES (5, 'will_w', 'Will', 'W', 'will@email.com', 'password5', 0);
 
 
 CREATE TABLE IF NOT EXISTS `BBY_37_location` (
@@ -54,8 +54,8 @@ CREATE TABLE IF NOT EXISTS `BBY_37_post` (
   FOREIGN KEY(location_id) REFERENCES BBY_37_location(location_id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`) VALUES (1, 1, '2022-05-10 13:23:44', 'Great property the landlord was always nice and very responsive.', 1);
-INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`) VALUES (2, 1, '2022-05-10 14:25:51', 'I lived there for 2 years, the neighbors were terrible and always loud. Other than that the landlord was great and everything worked.', 2);
-INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`) VALUES (3, 2, '2022-05-10 11:21:03', 'The location is great, a little close the the Skytrain so there is some background noise from the trains when they pass by.', 3);
-INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`) VALUES (4, 3, '2022-05-10 16:53:55', 'Awesome place, very peaceful and quiet.', 4);
-INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`) VALUES (5, 1, '2022-05-10 17:16:23', 'I enjoyed the house, but the landlord was unresponsive and never fixed anything, then they tried to increase myrent at the end of the year. Had to move out because I could not deal with them', 5);
+INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`, `photo1`) VALUES (1, 1, '2022-05-10 13:23:44', 'Great property the landlord was always nice and very responsive.', 1, '/assets/images/placeholder1.png');
+INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`, `photo1`) VALUES (2, 1, '2022-05-10 14:25:51', 'I lived there for 2 years, the neighbors were terrible and always loud. Other than that the landlord was great and everything worked.', 2, '/assets/images/placeholder1.png');
+INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`, `photo1`) VALUES (3, 2, '2022-05-10 11:21:03', 'The location is great, a little close the the Skytrain so there is some background noise from the trains when they pass by.', 3, '/assets/images/placeholder1.png');
+INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`, `photo1`) VALUES (4, 3, '2022-05-10 16:53:55', 'Awesome place, very peaceful and quiet.', 4, '/assets/images/placeholder1.png');
+INSERT INTO `BBY_37_post` (`post_id`, `user_id`, `date_created`, `content`, `location_id`, `photo1`) VALUES (5, 1, '2022-05-10 17:16:23', 'I enjoyed the house, but the landlord was unresponsive and never fixed anything, then they tried to increase myrent at the end of the year. Had to move out because I could not deal with them', 5, '/assets/images/placeholder1.png');
