dc0ead25b152a8ed0609e981cd8b5c9af820dd70
vagabondtypewriter <kieferthominquiries@gmail.com>
2022-05-26 10:16:07 -0700

    Final bugfixes on js and serverside code

--- js/unitView.js
+++ js/unitView.js
@@ -1,28 +1,22 @@
 "use strict";
-console.log('loaded');
+
 // define variables
 let editing = false;
 let editBox;
 let data;
 let options;
 
+// function for editing a review item
 function edit_init() {
   if (!editing) {
     editing = true;
 
-    // testing 
+    // stores the parent div of the review item
     var editingReview = (document.getElementById("editBtn").parentNode.id);
-    // CREATE EDIT DIV
-    // initial set up
+
+    // create edit box
     editBox = document.createElement("div");
     editBox.className = "editBox";
-    // editBox.style.backgroundColor = "blue";
-    // get content for the review
-    // editBox.innerHTML += document.getElementById("1").querySelector(".rev").innerHTML;
-    // make the review item invisible
-    // set review item to hidden
-    document.getElementById(editingReview).classList.add('hidden');
-    // editRow.classList.add("hidden");
     // richtext editor
     editBox.innerHTML += '<div id="richText"><form method="post"><textarea id="mytextarea">' + document.getElementById(editingReview).querySelector(".rev").innerHTML + ' </textarea></form></div>';
     // image collection
@@ -37,13 +31,24 @@ function edit_init() {
     editBox.innerHTML += "<div id='errMsg'></div>";
     // Position insertion
     document.querySelector("#reviews").insertAdjacentElement("afterbegin", editBox);
+    
+    // set review item to hidden
+    document.getElementById(editingReview).classList.add('hidden');
+    
   } 
 }
 
+// function to submit an edited review
 async function submit_data() {
+  // stores the parent div of the review item
   var editingReview = (document.getElementById("editBtn").parentNode.id);
+
+  // data to be sent as request to server
   data = {"content" : document.getElementById('mytextarea').value,
-                "post_id": editingReview}; 
+                "post_id": editingReview
+          }; 
+
+  // options for the serverside function
   options = {
     method: 'POST',
     headers: {
@@ -52,23 +57,37 @@ async function submit_data() {
     },
     body: JSON.stringify(data)
   }
+
+  // server response handling
   const response = await fetch('/submitEdit', options);
   const status = await response.json();
+
+  // successful update
   if (status.status == "success") {
-    // successful update
+    // hide edit box
     editBox.style.display = "none";
+    // update review item
     document.getElementById(editingReview).querySelector(".rev").innerHTML = data.content;
+    // make review item visible
     document.getElementById(editingReview).classList.remove('hidden');
+    // set editing to false
     editing = false;
   } 
+  // failed update
   if (status.status == "fail") {
-    // did not update (review is empty)
+    // did not update (review is empty or only invalid characters)
     document.getElementById('errMsg').innerHTML="*Error, review content invalid";
   }
 }
 
+// function to cancel an edit state
 async function cancel() {
+  // hide edit box
   editBox.style.display = "none";
-  document.getElementById("1").classList.remove('hidden');
+  // stores the parent div of review item
+  var editingReview = (document.getElementById("editBtn").parentNode.id);
+  // make review item visible
+  document.getElementById(editingReview).classList.remove('hidden');
+  // set editing to false
   editing = false;
 }
